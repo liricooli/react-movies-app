7fbe4c98a085fde654015bb6ffbd49c788b5b6c4
Oded Regev <odedre@gmail.com>
2024-10-27 16:31:55 +0200

    python ref

---
+++ docs/interview-python.py
@@ -0,0 +1,64 @@
+# Base URL
+# https://movie-database-alternative.p.rapidapi.com
+
+# Query Params
+# `s=` - search query
+# `r=` - response format (default value: `json`)
+# `page=` (optional) - page number (default value: 1) - 10 MAX results per page
+
+# Headers
+# x-rapidapi-host: movie-database-alternative.p.rapidapi.com
+# x-rapidapi-key: <ASK_FOR_KEY>
+
+# Example Request
+# https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1
+
+# Example Response
+# {
+#   "Search": [
+#     {
+#       "Title": "Avengers: Endgame",
+#       "Year": "2019",
+#       "imdbID": "tt4154796",
+#       "Type": "movie",
+#       "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+#     },
+#     {
+#       "Title": "Marvel Studios' Avengers: Endgame LIVE Red Carpet World Premiere",
+#       "Year": "2019",
+#       "imdbID": "tt10240638",
+#       "Type": "movie",
+#       "Poster": "https://m.media-amazon.com/images/M/MV5BNThjZDgwZTYtMjdmYy00ZmUyLTk4NTUtMzdjZmExODQ3ZmY4XkEyXkFqcGdeQXVyMjkzMDgyNTg@._V1_SX300.jpg"
+#     },
+#     {
+#       "Title": "Avengers: Endgame and the Latest Captain Marvel Outrage!!",
+#       "Year": "2019",
+#       "imdbID": "tt10025738",
+#       "Type": "movie",
+#       "Poster": "https://m.media-amazon.com/images/M/MV5BZjg2ZTM3OTgtY2ExMS00OGM4LTg3NDEtNjQ0MjJiZDFmMGFkXkEyXkFqcGdeQXVyMDY3OTcyOQ@@._V1_SX300.jpg"
+#     },
+#     {
+#       "Title": "Avengers Endgame: the Butt Plan",
+#       "Year": "2019",
+#       "imdbID": "tt10399328",
+#       "Type": "movie",
+#       "Poster": "https://m.media-amazon.com/images/M/MV5BNTQ1OWQzODktMTY3Zi00OTQxLWExOTYtZTNjZjY5ZTY4M2UyXkEyXkFqcGdeQXVyMTAzMzk0NjAy._V1_SX300.jpg"
+#     },
+#     {
+#       "Title": "Avengers: Endgame (2019)",
+#       "Year": "2019",
+#       "imdbID": "tt16416424",
+#       "Type": "movie",
+#       "Poster": "https://m.media-amazon.com/images/M/MV5BY2I4YzU4YTYtZDc3OC00NjcwLTlhM2ItNjlmYjNiMjFkMTRiXkEyXkFqcGdeQXVyMTE4NTEwNTky._V1_SX300.jpg"
+#     },
+#     {
+#       "Title": "Avengers: Endgame (2019) - Spoiler Full Review",
+#       "Year": "2019",
+#       "imdbID": "tt17978032",
+#       "Type": "movie",
+#       "Poster": "N/A"
+#     }
+#   ],
+#   "totalResults": "6",
+#   "Response": "True"
+# }
